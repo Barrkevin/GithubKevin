f1c8ba38fc2a1148b087dfbbf8516bcdda7167db
Barrkevin <Barrkevin@users.noreply.github.com>
2019-12-12 16:14:35 +0100

    maj

--- formulaire/js/jsform.js
+++ formulaire/js/jsform.js
@@ -33,16 +33,33 @@ $("document").on("submit", function(event, target){   // Va chercher le document
 */
 
 
-$("button").on("click", function(event, target){   // Va chercher le document button, et quand on clique dessus cela déclenche l'évenement ci-dessous
-    event.preventDefault(); //Veut dire ne raffraichît pas la page
-    checkEmptyInputs(); //Appelle la fonction dessous
+$("button").on("click", function(event, target){   // Va chercher le document button, un clique dessus déclenche l'évenement ci-dessous
+    event.preventDefault(); // Veut dire ne raffraichît pas la page
+    checkEmptyInputs(); // Appelle la fonction dessous
 })
 
 function checkEmptyInputs(){
-    $(".notempty").each(function (i, elt){ //notempty appelle la classe notempty dans le htlm, pour dire tel input est obligatoire, each veut dire pour tous
+    $(".notempty").each(function (i, elt){ // notempty appelle la classe notempty dans le html, pour dire tel input est obligatoire, each veut dire pour tous
         if(elt.value == ""){ // Si l'élément est vide fait ça :
         $(elt).toggleClass("border-danger"); // Déclenche le border danger
         $(elt).next().toggleClass("d-none"); // En dessous ne cache plus le small
         }
     })
 }
+
+function checkEmail(){
+    $(".verifemail").each(function) (i, elt){
+        if(elt.value = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/){
+        }
+        else
+            ($(elt).toggleClass("border-danger");
+        $(elt).next().toggleClass("d-none");
+    }
+}
+
+/*function checkMailFormat(){
+    let pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    let saisie = $(".verifemail")[0].value;
+    saisie = $(selector)[0].value;
+}*/
+
